918ea9a09c7773a3cd37dcf62d2105e91037766e
ricardoCH <liker251281@gmail.com>
2022-11-24 02:00:13 -0300

    creacion del modelo user y guardado en la bbdd, con encriptacion de la contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,38 @@
 const { response } = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
 
-const crearUsuario = (req, res = response) => {
-    const { name, email, password } = req.body
+const crearUsuario = async (req, res = response) => {
+    const { email, password } = req.body
 
-    return res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password,
-    })
+    try {
+        let user = await User.findOne({ email })
+
+        if ( user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario ya existe con ese correo'
+            })
+        }
+        user = new User(req.body)
+        
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync()
+        user.password = bcrypt.hashSync(password, salt)
+
+        await user.save()
+    
+        return res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
 }
 
 const loginUsuario = (req, res = response) => {
